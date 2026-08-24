02b4bb5d5dd31fbbb46f06b671c37719145080b6
lethanh2006 <thanhld2006.ptit@gmail.com>
2026-08-24 17:49:03 +0700

    Ký yêu cầu nội bộ tới dịch vụ lịch làm việc

--- src/modules/workschedule/workschedule.module.ts
+++ src/modules/workschedule/workschedule.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { WorkscheduleController } from './workschedule.controller';
 import { WorkscheduleService } from './workschedule.service';
+import { InternalRequestSignatureService } from '../../common/security/internal-request-signature.service';
 
 @Module({
   imports: [HttpModule],
   controllers: [WorkscheduleController],
-  providers: [WorkscheduleService],
+  providers: [WorkscheduleService, InternalRequestSignatureService],
 })
 export class WorkscheduleModule {}
